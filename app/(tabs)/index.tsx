af5cf6cb86319e92924e1e2e7afbb855bce11232
Marios Tzialidis <103110817+whoismarios@users.noreply.github.com>
2023-08-15 22:46:43 +0200

    Rick added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground, StyleSheet } from 'react-native';
+import { ImageBackground, Linking, Pressable, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
@@ -7,15 +7,20 @@ import React from 'react';
 import { styles } from './../stylesheets/homeStyleSheet';
 import { ScrollView } from 'react-native-gesture-handler';
 
+async function Ricky() {
+  // open a browser with youtube
+  const link: String = await Linking.openURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+}
+
 export default function TabOneScreen() {
   return (
     <>
       <View>
-        <ImageBackground resizeMode='cover' source={require('./../../assets/images/Light.png')} style={styles.backgroundImage}>
+        <ImageBackground resizeMode='cover' source={require('./../../assets/images/bike-blur.png')} style={styles.backgroundImage}>
 
-          <View style={styles.greating}>
+          <Pressable onPress={Ricky} style={styles.greating}>
             <Text style={styles.title}>Hey, Wobin!</Text>
-          </View>
+          </Pressable>
 
           <Text style={styles.whatsNew}>What's new?</Text>
 
