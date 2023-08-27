09fef2d626841d17b4f864b8dccabc2b9984a9cf
Marios Tzialidis <103110817+whoismarios@users.noreply.github.com>
2023-08-28 00:26:11 +0200

    Backend in Java Spring added. Only initialized and not setted up yet.

---
+++ Backend/BikeShare/src/main/java/com/example/BikeShare/BikeShareApplication.java
@@ -0,0 +1,13 @@
+package com.example.BikeShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeShareApplication.class, args);
+	}
+
+}
---
+++ Backend/BikeShare/src/test/java/com/example/BikeShare/BikeShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BikeShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
